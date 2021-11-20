5a9d675f0eb85cd7d16a53010404c9e0b856dabe
kuldeep meena <kuldeep.meena.delhi@gmail.com>
2021-11-21 04:09:25 +0530

    middleware

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,20 @@
 const express = require('express');
+
 const router = express.Router();
-const bookModel= require("../models/bookModel")
+
 const authorModel= require("../models/authorModel")
-const Controller= require("../controllers/Controller")
+const authorcontroller= require("../controllers/authorcontroller")
 
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
-router.post('/createBookUser', Controller.createBookUser );
-router.post('/createauthorUser', Controller.createauthorUser  );
-router.get('/getChetanData',  Controller.getChetanData );
-router.get('/updateTwoStatesPrice',  Controller.updateTwoStatesPrice  );
-router.get('/getAuthorName',  Controller.getAuthorName );
+
+router.post('/createAuthor', authorcontroller.createAuthor  );
+router.get('/getname', authorcontroller.getname  );
+router.get('/getnameandage', authorcontroller.getnameandage );
+
 
 
 module.exports = router;
