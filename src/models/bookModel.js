0a77b0e1c40f81ddb9698169dadfb7d7e515f5d8
kuldeep meena <kuldeep.meena.delhi@gmail.com>
2021-11-21 04:02:46 +0530

    mongodb

---
+++ src/models/bookModel.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose')
+
+const bookSchema = new mongoose.Schema({
+    
+
+        name:String,
+
+        author_id:{type:Number,required:true},
+
+        price:Number,
+
+        ratings:Number,
+
+    
+    
+}, {timestamps: true} )
+
+
+
+
+
+
+
+module.exports=mongoose.model('16_nov_collection_one',bookSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
+const bookModel= require("../models/bookModel")
+const authorModel= require("../models/authorModel")
+const Controller= require("../controllers/Controller")
+
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+
+router.post('/createBookUser', Controller.createBookUser );
+router.post('/createauthorUser', Controller.createauthorUser  );
+router.get('/getChetanData',  Controller.getChetanData );
+router.get('/updateTwoStatesPrice',  Controller.updateTwoStatesPrice  );
+router.get('/getAuthorName',  Controller.getAuthorName );
+
 
 module.exports = router;
